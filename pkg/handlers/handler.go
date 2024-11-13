a4ca088210c058e94195aa9090a700900018872f
Gurulingayya Hosmath <Gurulingayya.Hosmath1@ibm.com>
2024-11-13 16:39:38 +0530

    Initial commit

---
+++ pkg/handlers/handler.go
@@ -0,0 +1,62 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/hosmathguru/MyWebApplication/pkg/config"
+	"github.com/hosmathguru/MyWebApplication/pkg/models"
+	"github.com/hosmathguru/MyWebApplication/pkg/render"
+)
+
+// // Home is the handler for the home page
+// func Home(w http.ResponseWriter, req *http.Request) {
+// 	render.RenderTemplate(w, "home.page.tmpl")
+// }
+
+// // About is the handler for the about page
+// func About(w http.ResponseWriter, req *http.Request) {
+// 	render.RenderTemplate(w, "about.page.tmpl")
+// }
+
+// Repo the repository used by the handlers
+var Repo *Repository
+
+// Repository is the repository type
+type Repository struct {
+	App *config.AppConfig
+}
+
+// NewRepo creates a new repository
+func NewRepo(a *config.AppConfig) *Repository {
+	return &Repository{
+		App: a,
+	}
+}
+
+// NewHandlers sets the repository for the handlers
+func NewHandlers(r *Repository) {
+	Repo = r
+}
+
+// Home is the handler for the home page
+func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
+	remoteIP := r.RemoteAddr
+	m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
+
+	render.RenderTemplate(w, "home.page.tmpl", &models.TemplateData{})
+}
+
+// About is the handler for the about page
+func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
+	// perform some logic
+	stringMap := make(map[string]string)
+	stringMap["test"] = "Hello, again"
+
+	remoteIP := m.App.Session.GetString(r.Context(), "remote_ip")
+	stringMap["remote_ip"] = remoteIP
+
+	// send data to the template
+	render.RenderTemplate(w, "about.page.tmpl", &models.TemplateData{
+		StringMap: stringMap,
+	})
+}
